55dd9ff8afd67f09451e86b21b85fe83bd5710df
Amnok <ankitsingh80741@gmail.com>
2021-12-05 17:23:47 +0530

    bubbling & capturing added

---
+++ miscellaneous/bubbling-capturing.js
@@ -0,0 +1,17 @@
+
+// const child = document.querySelector('#child');
+// child.addEventListener('click', () => {
+//     console.log('from child');
+// })
+
+function fromChild () {
+    console.log('from child');  
+}
+
+function fromParent () {
+    console.log('from parent');  
+}
+
+function fromGrandParent () {
+    console.log('from grand parent');  
+}
