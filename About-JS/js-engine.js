f8199c4cc5f89ad7def81c25f4c457e12ead136d
Amnok <ankitsingh80741@gmail.com>
2021-12-16 17:30:49 +0530

    js engine

---
+++ About-JS/js-engine.js
@@ -0,0 +1,6 @@
+// what is JRE
+// -> JRE is the combination fo javascript engine, event loop, callback queue, web apis
+
+// compiler vs interpreter
+// is JS compiled or interpreted
+// what is JIT compilation
