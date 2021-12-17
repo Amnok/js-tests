ee8bab06aec5ee16f68bccccf15c9f81c9622a8e
Amnok <ankitsingh80741@gmail.com>
2021-12-17 14:54:59 +0530

    some more info

--- objects/basic.js
+++ objects/basic.js
@@ -1,6 +1,8 @@
 // difference b/w shallow vs deep copy
 // how is this calculated in JS
 // how may types of bindings are there in JS
+// what are constructor functions ? with example
+
 // output for below :-
 
 // let user = {
@@ -10,4 +12,31 @@
 //     }
 // }
 // console.log(user.getName());
+
+//  output for below
+
+// function sayHi() {
+//     console.log(this);
+// }
+
+// output for below :
+
+// let user = {
+//     firstName: 'test',
+//     sayHi() {
+//         let ar = () => console.log(this.firstName);
+//         ar();
+//     }
+// }
+// user.sayHi();
+
+// output for below 
+
+// function makeUser() {
+//     return {
+//         name: 'test',
+//         ref: this
+//     }
+// }
+// let user = makeUser();
 
